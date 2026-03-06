3eccd4fe527f998f2a8551a1dc1a2362e4ac28d0
gzezFISHER <fisher070305@163.com>
2026-03-07 01:08:22 +0800

    various improvements

--- Vigenere_cipher.py
+++ Vigenere_cipher.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import defaultdict
 def vigenere_encryption(plaintext, key):
     ciphered_list=[-1]*len(plaintext)
     alphabet="abcdefghijklmnopqrstuvwxyz"
@@ -24,12 +24,10 @@ def vigenere_encryption(plaintext, key):
     if is_valid:
         ciphered="".join(ciphered_list)
         return ciphered
-    else:
-        return "Error: invalid plaintext or key."
+    raise RuntimeError("Invalid plaintext or key.")
 
 def vigenere_decryption(ciphered, epsilon=0.009):
     IOC=0.065
-    random_IOC=0.0385
     alphabet="abcdefghijklmnopqrstuvwxyz"
     alphabet_dict={}
     reverse_dict={}
@@ -39,17 +37,17 @@ def vigenere_decryption(ciphered, epsilon=0.009):
     def get_len(ciphered):
         len_percentage={}
         for i in range(1, len(alphabet)):
-            all_result=[[] for _ in range(i)]
+            all_result=[defaultdict(int) for _ in range(i)]
             for j in range(len(ciphered)):
-                all_result[j%i].append(ciphered[j])
+                all_result[j%i][ciphered[j]]+=1
             matched_num=0
             total_num=0
             for result in all_result:
-                for m in range(len(result)):
-                    for n in range(m+1, len(result)):
-                        total_num+=1
-                        if result[m]==result[n]:
-                            matched_num+=1
+                total_count = 0
+                for count in result.values():
+                    matched_num+=count*(count-1)//2
+                    total_count+=count
+                total_num+=total_count*(total_count-1)//2
             percentage=matched_num/total_num
             len_percentage[i]=abs(percentage-IOC)
         for index, per in len_percentage.items():
@@ -64,33 +62,59 @@ def vigenere_decryption(ciphered, epsilon=0.009):
         all_result=[[] for _ in range(length)]
         counter=0
         letter_frequencies = {
-    'e': 0.12702,
-    't': 0.09056,
-    'a': 0.08167,
-    'o': 0.07507,
-}
+            'a': 0.08167,
+            'b': 0.01492,
+            'c': 0.02782,
+            'd': 0.04253,
+            'e': 0.12702,
+            'f': 0.02228,
+            'g': 0.02015,
+            'h': 0.06094,
+            'i': 0.06966,
+            'j': 0.00153,
+            'k': 0.07720,
+            'l': 0.04025,
+            'm': 0.02406,
+            'n': 0.06749,
+            'o': 0.07507,
+            'p': 0.01929,
+            'q': 0.00095,
+            'r': 0.05987,
+            's': 0.06327,
+            't': 0.09056,
+            'u': 0.02758,
+            'v': 0.00978,
+            'w': 0.02360,
+            'x': 0.00150,
+            'y': 0.01974,
+            'z': 0.00074
+        }
         for letter in ciphered:
             all_result[counter%length].append(letter)
             counter+=1
-        def get_top_4_letters(some_list):
-            some_dict={}
-            some_percentage_dict={}
-            for letter in alphabet:
-                some_dict[letter]=0
-                some_percentage_dict[letter]=0
-            for letter in some_list:
-                some_dict[letter]+=1
-            for letter in alphabet:
-                some_percentage_dict[letter]=some_dict[letter]/len(some_list)
-            top_4_letters=sorted(some_percentage_dict, key=some_percentage_dict.get, reverse=True)[:4]
-            return top_4_letters
+
+        def get_best_shift(sub_cipher):
+            total=len(sub_cipher)
+            observed_freq = defaultdict(float)
+            for letter in sub_cipher:
+                observed_freq[letter]+=1/total
+            if total==0:
+                return 0
+            best_shift=0
+            min_diff=float('inf')
+            for shift in range(len(alphabet)):
+                diff_sum=0
+                for letter in alphabet:
+                    shifted_letter=alphabet[(reverse_dict[letter]+shift)%len(alphabet)]
+                    diff_sum+=(observed_freq[shifted_letter] - letter_frequencies[letter]) ** 2
+                if diff_sum<min_diff:
+                    min_diff=diff_sum
+                    best_shift=shift
+            return best_shift
+
         for i in range(length):
-            top_4_letters=get_top_4_letters(all_result[i])
-            move_steps_num=[]
-            for j in range(len(letter_frequencies)):
-                move_steps_num.append((reverse_dict[top_4_letters[j]]-reverse_dict[list(letter_frequencies.keys())[j]])%len(alphabet))
-            mode=Counter(move_steps_num).most_common(1)[0][0]
-            key[i]=alphabet_dict[mode]
+            shift = get_best_shift(all_result[i])
+            key[i] = alphabet_dict[shift]
         return key
     key_length=get_len(ciphered)
     key=get_key(ciphered, key_length)
